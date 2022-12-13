1760ef0a031d1cd2e1e0d4097b7667e5df73b4a8
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-13 16:17:18 -0300

    refactor: props importation

--- src/components/DefaultButton.jsx
+++ src/components/DefaultButton.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function DefaultButton(props) {
+  const { type, clickButton, text } = props;
+
   return (
     <button
       className={ props.className }
-      type={ props.type || "button" }
-      onClick={ props.clickInput }
+      type={ type || "button" }
+      onClick={ clickButton }
     >
-      { props.text }
+      { text }
     </button>
   );
 }
@@ -17,7 +19,7 @@ DefaultButton.propTypes = {
   text: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   className: PropTypes.string.isRequired,
-  clickInput: PropTypes.func.isRequired,
+  clickButton: PropTypes.func.isRequired,
 };
 
 export default DefaultButton;
