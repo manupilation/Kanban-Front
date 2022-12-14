74ecaedb304a7d65051f7a7a2379a47b63bd3113
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-14 14:05:00 -0300

    feat: task page

---
+++ src/pages/TasksPage/TaskPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TaskHeader from "../../components/TaskHeader/TaskHeader";
+import TaskBody from "../../components/TaskBody/TaskBody";
+
+function TaskPage() {
+  return (
+    <div>
+      <TaskHeader />
+      <TaskBody />
+    </div>
+  );
+}
+
+export default TaskPage;
