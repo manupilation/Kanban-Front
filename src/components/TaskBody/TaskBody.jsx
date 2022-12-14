59b22334fabd7c3be6626fcd2cc1dfafceec30e0
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-14 14:05:27 -0300

    feat: task body

---
+++ src/components/TaskBody/TaskBody.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function TaskBody() {
+  return (
+    <main>
+      <div>
+        <p>Lista de tarefas:</p>
+      </div>
+
+      <section>
+        
+      </section>
+    </main>
+  );
+}
+
+export default TaskBody;
