333ae0dee8ac55733ddebc8e295620ac86ba7a9f
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-15 19:00:27 -0300

    feat: edit Task

--- src/components/TaskCard/taskCard.jsx
+++ src/components/TaskCard/taskCard.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import styles from "./styles.module.scss";
+import UserContext from "../../context/userContext";
 
 function TaskCard({ task, date, status, _id }) {
+  const { setUpdateData, setUpdating } = useContext(UserContext);
+  const dateFormat = date.replace("T", " ").replace(".000Z", "");
+
   function statusName() {
     switch(status) {
     case "in progress":
@@ -19,16 +23,34 @@ function TaskCard({ task, date, status, _id }) {
     }
   }
 
+  function handleEditTask(event) {
+    event.preventDefault();
+
+    setUpdating(true);
+
+    setUpdateData((prev) => {
+      return {
+        ...prev,
+        date: dateFormat,
+        task: task,
+        status: status,
+        _id: _id
+      };
+    });
+  }
+
+
   return (
     <div className={styles.taskCardContainer}>
       <div>
-        <div>
+        <div onClick={handleEditTask}>
           <img src=""/>
+          <a>Editar</a>
         </div>
         <h2>{task}</h2>
       </div>
       <p>{statusName()}</p>
-      <p>{date.replace("T", " ").replace(".000Z", "")}</p>
+      <p>{dateFormat}</p>
       <p hidden={true}>{_id}</p>
     </div>
   );
