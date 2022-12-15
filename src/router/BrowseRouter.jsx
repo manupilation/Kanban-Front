29d47f3b1350f196d11902ca92c350ba8ce0a624
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-15 02:49:27 -0300

    feat: Tasks route

--- src/router/BrowseRouter.jsx
+++ src/router/BrowseRouter.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import RegisterPage from "../pages/Register/RegisterPage";
-import TaskBody from "../components/TaskBody/TaskBody";
+import TaskPage from "../pages/TasksPage/TaskPage";
 
 function AppRouter() {
+
   return(
     <HashRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />}/>
         <Route path="/register" element={<RegisterPage />}/>
-        <Route path="tasks" element={ <TaskBody /> } />
+        <Route path="/tasks" element={ <TaskPage /> } />
         <Route path="/" element={<Navigate to="/login" />}/>
       </Routes>
     </HashRouter>
