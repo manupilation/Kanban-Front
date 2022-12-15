271974001d5422a484713922497f3b9b368db2ae
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-15 02:50:02 -0300

    feat: Task body

--- src/components/TaskBody/TaskBody.jsx
+++ src/components/TaskBody/TaskBody.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useContext, memo } from "react";
+import UserContext from "../../context/userContext";
+import { Navigate } from "react-router-dom";
+import TaskCard from "../TaskCard/taskCard";
+import UpdateForm from "../UpdateForm/UpdateForm";
 
 function TaskBody() {
-  return (
-    <main>
-      <div>
-        <p>Lista de tarefas:</p>
-      </div>
+  const { userData } = useContext(UserContext);
 
-      <section>
-        
-      </section>
-    </main>
-  );
+  if("error" in userData) {
+    return <Navigate to="/login"/>;
+  } else {
+    return (
+      <main>
+        <div>
+          <p>Lista de tarefas:</p>
+        </div>
+
+        <section>
+          <UpdateForm />
+          { userData.tasks?.map((t, i) => (
+            <TaskCard
+              task={t.task}
+              key={i}
+              status={t.status}
+              date={t.date}
+              _id={t._id}
+            />
+          ))}
+        </section>
+      </main>
+    );
+  }
 }
 
-export default TaskBody;
+export default memo(TaskBody);
