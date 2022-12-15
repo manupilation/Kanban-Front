9da1699e57f082b10ded0e0a0a01519f507627d6
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-15 19:01:31 -0300

    feat: task header classes and style import

--- src/components/TaskHeader/TaskHeader.jsx
+++ src/components/TaskHeader/TaskHeader.jsx
@@ -1,19 +1,20 @@
 import React, { useContext } from "react";
 import UserContext from "../../context/userContext";
 import DefaultButton from "../DefaultButton";
+import styles from "./styles.module.scss";
 
 function TaskHeader() {
   const { userData } = useContext(UserContext);
 
   return(
-    <header>
+    <header className={styles.headerContainer}>
       <h1>Boas vindas, { userData.user + "!" || "Visitante!" }</h1>
-      <div>
+      <div className={styles.menuButton}>
         <DefaultButton
           type="button"
           text="MENU"
         />
-        <ul>
+        <ul className={styles.menuList}>
           <li><a>SAIR</a></li>
         </ul>
       </div>
